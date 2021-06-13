1ccad4a8a699324cd19abae4adce77a3254641e9
Németh Máté <isa0o0@icloud.com>
2021-06-13 19:28:50 +0200

    fafafa

---
+++ range/js/script.js
@@ -0,0 +1,17 @@
+function r1(){
+    var w = document.getElementById('w').value;
+    document.getElementById('er').innerHTML=w;
+    document.getElementById('kep').width=w;
+}
+function szin(x){
+    document.body.style.background= x;
+    if (x == 'black'){
+        document.body.style.color = "white";
+        document.body.style.fontSize = "200%";
+        document.body.style.fontFamily = "Arial, Helvetica, sans-serif"
+    }else{
+        document.body.style.color = "black";
+        document.body.style.fontSize = "100%";
+        document.body.style.fontFamily = "Arial, Helvetica, sans-serif"
+    }
+}
